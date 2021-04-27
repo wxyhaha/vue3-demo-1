eb90cf151b7f7178104b0cc03ed921f4fa1ac1ca
joel <1666464510@qq.com>
2021-04-27 16:09:34 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Button @click="open">hi</Button>
+  <hr/>
+  <Switch v-model:value="switchValue"/>
+  <hr/>
+  <Tabs v-model:selected="tabSelected">
+    <Tab title="tab1">内容1</Tab>
+    <Tab title="tab2">内容2</Tab>
+  </Tabs>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// 'gulu-ui-2021' 是我下载的老师最终版代码编译上传的库
+// 'gulu-ui-1' 是老师上传的库
+// 'flash-shan-ui' 是我的库
+import {Button,Switch,Dialog,openDialog,Tabs,Tab} from 'gulu-ui-2021'
+import 'gulu-ui-2021/dist/lib/gulu.css'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name:'App',
+  components: {Button,Switch,Dialog,Tabs,Tab},
+  data(){
+    return{
+      switchValue:false,
+      tabSelected:'tab1'
+    }
+  },
+  methods:{
+    open(){
+      openDialog({title:'标题',content:'内容'})
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
